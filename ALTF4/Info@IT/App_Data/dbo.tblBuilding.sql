1612a7e372d7bf3a447731e6d9e7a6665545e47d
Janak <janakdaya@gmail.com>
2019-09-06 23:33:13 +0200

    Checked for missing sql, most are there, will take care of that tomorrow.

---
+++ ALTF4/Info@IT/App_Data/dbo.tblBuilding.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[tblBuilding]
+(
+	[Building] VARCHAR(3) NOT NULL PRIMARY KEY, 
+    [BuildingName] VARCHAR(50) NOT NULL
+)
---
+++ ALTF4/Info@IT/App_Data/dbo.tblBuildingBlock.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[tblBuildingBlock] (
+    [BuildingBlock] VARCHAR (1)  NOT NULL,
+    [BuildingName]  VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([BuildingBlock] ASC)
+);
+
