81a7a12e3127a2aeb1ba93f9512d20cf38855954
Conrad <conraddup@outlook.com>
2019-09-30 22:06:38 +0200

    Done

--- ALTF4/Info@IT/UserControls/ucRequest.Designer.cs
+++ ALTF4/Info@IT/UserControls/ucRequest.Designer.cs
@@ -194,25 +194,25 @@
 			// 
 			this.cmbTime.FormattingEnabled = true;
 			this.cmbTime.Items.AddRange(new object[] {
-            "08:00",
-            "08:30",
-            "09:00",
-            "09:30",
-            "10:00",
-            "10:30",
-            "11:00",
-            "11:30",
-            "12:00",
-            "12:30",
-            "13:00",
-            "13:30",
-            "14:00",
-            "14:30",
-            "15:00",
-            "15:30",
-            "16:00",
-            "16:30"});
-			this.cmbTime.Location = new System.Drawing.Point(15, 77);
+            "08:00:00",
+            "08:30:00",
+            "09:00:00",
+            "09:30:00",
+            "10:00:00",
+            "10:30:00",
+            "11:00:00",
+            "11:30:00",
+            "12:00:00",
+            "12:30:00",
+            "13:00:00",
+            "13:30:00",
+            "14:00:00",
+            "14:30:00",
+            "15:00:00",
+            "15:30:00",
+            "16:00:00",
+            "16:30:00"});
+            this.cmbTime.Location = new System.Drawing.Point(15, 77);
 			this.cmbTime.Name = "cmbTime";
 			this.cmbTime.Size = new System.Drawing.Size(116, 23);
 			this.cmbTime.TabIndex = 25;
