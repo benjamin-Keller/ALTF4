ca056085140e17cb94df17d94cc2f5ffd33d492b
Ben <s219611807@mandela.ac.za>
2019-10-03 15:00:06 +0200

    Test

--- ALTF4/BAL/GeneralLibrary.cs
+++ ALTF4/BAL/GeneralLibrary.cs
@@ -5,12 +5,7 @@ namespace DAL
 	class GeneralLibrary
 	{
 		//This is where we add any Methods that we need
-		DataAccessLayer dal = new DataAccessLayer();
-		public DataTable PopulateCmbBuilding()
-		{
-
-			return dal.PopulateCmbBuilding();
-		}
+		
 
 	}
 }
