0b652eda5a8ef38269f89b7c7195a1637e5e1b8f
Conrad <conraddup@outlook.com>
2019-10-17 18:23:32 +0200

    Flat button department

---
+++ Info@IT Website/Info@IT/Script/Scripts.js
@@ -0,0 +1,7 @@
+﻿function openFlatButtonDepartment() {
+
+    if (document.getElementById("addButtonDepartment").style.display == "none")
+        document.getElementById("addButtonDepartment").style.display = "block";
+    else
+        document.getElementById("addButtonDepartment").style.display = "none";
+}
