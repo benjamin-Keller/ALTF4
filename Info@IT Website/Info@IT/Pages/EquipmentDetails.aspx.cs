0969c42347d4a6a1e36b814348dc6e0cb9c314e5
Viral99 <rianvalenzuela99@gmail.com>
2019-10-28 16:36:17 +0200

    Added code behind for EquipmentDetails

--- Info@IT Website/Info@IT/Pages/EquipmentDetails.aspx.cs
+++ Info@IT Website/Info@IT/Pages/EquipmentDetails.aspx.cs
@@ -18,11 +18,72 @@ namespace Info_IT.Pages
 
         protected void dgEquipmentType_SelectedIndexChanged(object sender, EventArgs e)
         {
+            try
+            {
+                DAL.EquipmentTypeClass equipmentTypeClass = new DAL.EquipmentTypeClass(int.Parse(dgEquipmentType.SelectedItem.Cells[1].Text.ToString()));
 
+                var values = bll.SelectedForUpdateEquipmentType(equipmentTypeClass);
+
+                txtDescription.Text = values.Rows[0].Table.Rows[0].ItemArray[1].ToString();
+            }
+#pragma warning disable CS0168 // The variable 'b' is declared but never used
+            catch (Exception b)
+#pragma warning restore CS0168 // The variable 'b' is declared but never used
+            {
+
+            }
         }
 
         protected void dgEquipmentType_Load1(object sender, EventArgs e)
         {
+            dgEquipmentType.DataSource = bll.GetEquipmentType();
+            dgEquipmentType.DataBind();
+        }
+        protected void btnManageAdd(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                //Error for input string not found
+                DAL.EquipmentTypeClass equipment = new DAL.EquipmentTypeClass(txtDescription.Text);
+                int x = bll.AddEquipmentType(equipment);
+
+                if (x > 0)
+                {
+                    txtDescription.Text = "";
+                }
+                else
+                {
+                }
+            }
+            catch
+            {
+            }
+
+            dgEquipmentType.DataSource = bll.GetEquipmentType();
+            dgEquipmentType.DataBind();
+        }
+
+        protected void btnManageUpdate(object sender, EventArgs e)
+        {
+            try
+			{
+				DAL.EquipmentTypeClass equipment = new DAL.EquipmentTypeClass(DAL.EquipmentTypeClass.EquipTypeCode, txtDescription.Text);
+				int x = bll.UpdateEquipmentType(equipment);
+
+				if (x > 0)
+				{
+                    txtDescription.Text = "";
+				}
+				else
+				{
+				}
+			}
+			catch
+			{
+			}
+
             dgEquipmentType.DataSource = bll.GetEquipmentType();
             dgEquipmentType.DataBind();
         }
